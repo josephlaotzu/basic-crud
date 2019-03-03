0a56cb064707a10e1309c99c5c140df1753182f6
josephlaotzu <josephlao.1398@gmail.com>
2019-03-03 14:23:20 +0800

    contact and address info add and remove functionality

--- src/app/employees/employee-detail/employee-detail.component.ts
+++ src/app/employees/employee-detail/employee-detail.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EmployeesService } from '../employees.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-employee-detail',
   templateUrl: './employee-detail.component.html',
@@ -18,10 +20,9 @@ export class EmployeeDetailComponent implements OnInit {
   id: string;
   loading = true;
   constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router,
-    private employeeService: EmployeesService, private a: AngularFirestore) { }
+    private employeeService: EmployeesService, private a: AngularFirestore, private location: Location) { }
 
   ngOnInit() {
-
     this.id = this.route.snapshot.paramMap.get('id');
     // if edit get current employee data
     if (this.id) {
@@ -38,28 +39,19 @@ export class EmployeeDetailComponent implements OnInit {
   }
 
   buildForm(data) {
+    data.contactInfo = data.contactInfo ? data.contactInfo : [{value: ''}]
+    data.addressInfo = data.addressInfo ? data.addressInfo : [{}]
     this.employeeForm = this.formBuilder.group({
       firstname: [data.firstname || '', Validators.required],
       lastname: [data.lastname || '', Validators.required],
       middlename: [data.middlename || '', Validators.required],
-      birthday: [data.birthday || '', Validators.required],
+      birthday: [this.formatDateValue(data.birthday) || '', Validators.required],
       gender: [data.gender || '', Validators.required],
       maritalStatus: [data.maritalStatus || '', Validators.required],
       position: [data.position || '', Validators.required],
-      dateHired: [data.dateHired || '', Validators.required],
-      contactInfo: this.formBuilder.array([
-        this.formBuilder.group({
-          value: ['', ''],
-          isPrimary: ['', '']
-        })
-      ]),
-      addressInfo: this.formBuilder.array([
-        this.formBuilder.group({
-          street: ['', ''],
-          city: ['', ''],
-          isPrimary: ['', '']
-        })
-      ]),
+      dateHired: [this.formatDateValue(data.dateHired) || '', Validators.required],
+      contactInfo: this.buildFormArray(data.contactInfo),
+      addressInfo: this.buildFormArray(data.addressInfo)
     });
 
     this.contactInfoArray = this.employeeForm.get('contactInfo') as FormArray;
@@ -68,7 +60,6 @@ export class EmployeeDetailComponent implements OnInit {
 
   onSave(f: NgForm) {
     this.submitted = true;
-
     if (f.valid) {
       f.value.birthday = new Date(f.value.birthday); // convert to firebase supported timestamp value
       f.value.dateHired = new Date(f.value.dateHired);
@@ -79,4 +70,62 @@ export class EmployeeDetailComponent implements OnInit {
     }
   }
 
+  goBack() {
+    this.location.back();
+  }
+
+  buildFormArray(arr) {
+    const formarray = () => {
+      let holder = [];
+      arr.forEach(item => {
+        if (item.hasOwnProperty('value')) {
+          holder.push(this.formBuilder.group({
+            value: [item.value || '', ''],
+            isPrimary: [item.isPrimary || 'true', '']
+          }))
+        } else {
+          holder.push(this.formBuilder.group({
+            street: [item.street || '', ''],
+            city: [item.city || '', ''],
+            isPrimary: [item.isPrimary || 'true', '']
+          }))
+        }
+      });
+      return holder;
+    };
+    return this.formBuilder.array(formarray());
+  }
+
+  formatDateValue(date) {
+    date = date ? date.toDate() : new Date();
+    return (date.getFullYear() + '-' + ("0" + date.getDate()).slice(-2) + "-" + ("0" + (date.getMonth() + 1)).slice(-2));
+  }
+
+  addContacts() {
+    this.contactInfoArray.push(
+      this.formBuilder.group({
+        value: ['', '',],
+        isPrimary: ['false', '']
+      })
+    );
+  }
+
+  addAddress() {
+    this.addressInfoArray.push(
+      this.formBuilder.group({
+        street: ['', ''],
+        city: ['', ''],
+        isPrimary: ['false', '']
+      })
+    );
+  }
+
+
+  removeContact(index) {
+    this.contactInfoArray.removeAt(index);
+  }
+
+  removeAdress(index) {
+    this.addressInfoArray.removeAt(index);
+  }
 }
